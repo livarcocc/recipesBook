bb5221f41f26ad479762b1e9145c2499d6672e23
Livar Cunha <livar@live.com>
2014-12-15 16:17:42 -0800

    Adding cucumber, phantomsjs and web drive with a initial test to verify cucumber is properly setup

---
+++ tests/features/step_definitions/sharedSteps.js
@@ -0,0 +1,15 @@
+var sharedSteps = module.exports = function(){
+  this.World = require('../support/world');
+
+  this.Given(/^I am on the home page$/, function(next) {
+    this.visit('/', next);
+  });
+
+  this.Then(/^I should see "([^"]*)"$/, function(text, next) {
+    this.browser.elementByCss('body', function (err, el) {
+      el.text(function(err, actualText) {
+        actualText.should.containEql(text);
+      });
+    });
+  });
+}
---
+++ tests/features/support/world.js
@@ -0,0 +1,37 @@
+var HTML5  = require('html5')
+  , should = require('should')
+  , server = require('../../../server')
+  , wd = require('wd');
+
+var browser = wd.remote();
+
+// log status output from web driver
+browser.on('status', function(info){
+  console.log('\x1b[36m%s\x1b[0m', info);
+});
+
+// log commands from web driver
+browser.on('command', function(meth, path, data){
+  console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path, data || '');
+});
+
+module.exports = function() {
+  this.World = function World(callback) {
+    this.browser = browser;
+
+    this.page = function (path) {
+      return "http://localhost:" + server.app.address().port + path
+    };
+
+    this.visit = function (url, callback) {
+      this.browser.get(this.page(url), function (err, browser, status) {
+        callback(err, browser, status);
+      });
+    };
+
+    // run the callback when we are done do cucumber knows we are ready
+    this.browser.init({browserName: 'chrome'}, function() {
+      callback();
+    });
+  }
+};
