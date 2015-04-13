f5d55334bb9e678b42f5487451965f6ce4f356dd
Livar Cunha <livar@live.com>
2015-04-12 19:24:15 -0700

    Adding a login page by itself. Not fully working yet. But I want it to be visible in the remote branch

--- public/app/account/rbLoginController.js
+++ public/app/account/rbLoginController.js
@@ -1,4 +1,4 @@
-angular.module(ApplicationConfiguration.applicationModuleName).controller("rbLoginController", function($scope, rbNotifier, rbIdentity, rbAuthentication) {
+angular.module(ApplicationConfiguration.applicationModuleName).controller("rbLoginController", function($scope, rbNotifier, rbIdentity, rbAuthentication, $location) {
   $scope.login = function(username, password) {
     $scope.identity = rbIdentity;
 
@@ -6,7 +6,9 @@ angular.module(ApplicationConfiguration.applicationModuleName).controller("rbLog
     authenticate.then(function() {
       rbNotifier.success('You have successfully signed in.');
     }, function (message) {
+      $scope.errorMessage = message;
       rbNotifier.error(message);
+      $location.path('/login');
     });
   };
 });
--- public/app/app.js
+++ public/app/app.js
@@ -6,6 +6,8 @@ angular.module(ApplicationConfiguration.applicationModuleName,
 angular.module(ApplicationConfiguration.applicationModuleName).config(function ($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', { templateUrl: '/partials/main/main', controller: 'mainCtrl'});
+  $routeProvider
+    .when('/login', {templateUrl: '/partials/account/login', controller: 'rbLoginController'})
 });
 
 angular.module(ApplicationConfiguration.applicationModuleName).controller('mainCtrl', function ($scope) {
